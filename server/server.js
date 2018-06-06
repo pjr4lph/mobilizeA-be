939cb8aa54b9435d6eee96c04c6082f23ad453b2
ralph salazar <ralph.slzr@gmail.com>
2018-06-06 16:09:52 -0400

    cleaned up some comments, not working styles

--- server/server.js
+++ server/server.js
@@ -17,10 +17,11 @@ db.once('open', () => {
   console.log('Connected to Database');
 });
 
-// this is used to simply check what is being asked for - for development only
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, './../client')));
 
+// this is used to simply check what is being asked for - for development only
 app.use((req, res, next) => {
   console.log(req.method, req.url, req.body);
   next();
